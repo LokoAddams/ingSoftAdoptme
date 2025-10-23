187c0efda59ab4e99ffab15dee993cb078b44dbc
Vanessa745 <vanessa.canaviri@ucb.edu.bo>
2025-10-23 00:18:59 -0400

    feat: Implementación de verMascotas en la interfaz

--- src/presenter.js
+++ src/presenter.js
@@ -1,8 +1,10 @@
+import { verMascotas } from "./verMascotas";
+
 const form = document.querySelector("#verMasc-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  div.innerHTML = "<p>No hay mascotas disponibles.</p>";
+  div.innerHTML = "<p>" + verMascotas([]) + "</p>";
 });
