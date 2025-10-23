062cb97905737e4c3a64f11e54718e1a34aecc4f
Vanessa745 <vanessa.canaviri@ucb.edu.bo>
2025-10-23 01:05:39 -0400

    feat: Ver nombre, imagen, facilitador de varias mascotas que estan en la lista

--- src/verMascotas.test.js
+++ src/verMascotas.test.js
@@ -34,3 +34,9 @@ describe("Ver nombre, imagen y facilitador de una Mascota.", () => {
     expect(verMascotas([["Apolo", "https://www.mediterraneannatural.com/wp-content/uploads/2019/08/Guia-completa-de-las-razas-de-perros-Pit-Bull-Terrier-Americano-3.jpg", "Centro Patitas al rescate"]])).toEqual([["Apolo", "https://www.mediterraneannatural.com/wp-content/uploads/2019/08/Guia-completa-de-las-razas-de-perros-Pit-Bull-Terrier-Americano-3.jpg", "Centro Patitas al rescate"]]);
   });
 });
+
+describe("Ver nombre, imagen y facilitador de varias Mascotas.", () => {
+  it("deberia mostrar el nombre, imagen y facilitador de las mascotas que están en la lista.", () => {
+    expect(verMascotas([["Apolo", "https://www.mediterraneannatural.com/wp-content/uploads/2019/08/Guia-completa-de-las-razas-de-perros-Pit-Bull-Terrier-Americano-3.jpg", "Centro Patitas al rescate"], ["Perlita", "https://apupabove.com/cdn/shop/articles/Chihuahua_2ab3f5c4-9781-48ed-8119-7f780902c133_1200x1200.jpg?v=1742407300", "Rescatista María Prado"], ["Bruno", "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/MiniDachshund1_wb.jpg/330px-MiniDachshund1_wb.jpg", "Albergue Huellitas Libres"]])).toEqual([["Apolo", "https://www.mediterraneannatural.com/wp-content/uploads/2019/08/Guia-completa-de-las-razas-de-perros-Pit-Bull-Terrier-Americano-3.jpg", "Centro Patitas al rescate"], ["Perlita", "https://apupabove.com/cdn/shop/articles/Chihuahua_2ab3f5c4-9781-48ed-8119-7f780902c133_1200x1200.jpg?v=1742407300", "Rescatista María Prado"], ["Bruno", "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/MiniDachshund1_wb.jpg/330px-MiniDachshund1_wb.jpg", "Albergue Huellitas Libres"]]);
+  });
+});
