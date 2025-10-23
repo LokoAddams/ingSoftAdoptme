033a05d83e79219071830aa622fad8d8839ead7d
Adrian Claros <166774927+AdrianCV2345@users.noreply.github.com>
2025-10-22 23:29:55 -0400

    feat: Se creo la funcionalidad de abrir el selector de archivos.

--- cypress/e2e/DescripcionBasica.cy.js
+++ cypress/e2e/DescripcionBasica.cy.js
@@ -13,4 +13,11 @@ describe('File Upload Test', () => {
         cy.get('input[type="file"]').attachFile(fileName)
         cy.get('#archivoSeleccionado').should('contain.text', `Archivo seleccionado: ${fileName}`)
     });
+    it('Deberia mostrar mensaje cuando no hay archivo seleccionado', () => {
+        cy.get('#archivoSeleccionado').should('contain.text', 'No hay archivo seleccionado')
+    });
+    it('Deberia abrir el selector de archivos al hacer click en el boton', () => {
+        cy.get('#subirDocumentoBtn').click()
+        cy.get('input[type="file"]').should('be.visible')
+    });
 })
--- src/presenter.js
+++ src/presenter.js
@@ -8,3 +8,27 @@ function mostrarArchivoSeleccionado() {
   const archivo = archivoInput && archivoInput.files && archivoInput.files[0];
   archivoSeleccionadoDiv.innerText = archivo ? `Archivo seleccionado: ${archivo.name}` : 'No hay archivo seleccionado';
 }
+
+function initPresenter() {
+  if (botonSubir && archivoInput) {
+    botonSubir.addEventListener('click', () => {
+      archivoInput.click();
+    });
+  }
+
+  if (archivoInput && archivoSeleccionadoDiv) {
+    archivoInput.addEventListener('change', mostrarArchivoSeleccionado);
+  }
+
+  mostrarArchivoSeleccionado();
+
+  return {
+    botonSubirPresent: !!botonSubir,
+    archivoInputPresent: !!archivoInput,
+    archivoSeleccionadoDivPresent: !!archivoSeleccionadoDiv,
+  };
+}
+
+const _initResult = initPresenter();
+
+export { mostrarArchivoSeleccionado, initPresenter };
