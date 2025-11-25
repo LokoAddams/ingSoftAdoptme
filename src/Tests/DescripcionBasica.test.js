eb918864698f29ca7f2df4481e21116e7b0d3657
Adrian Claros <166774927+AdrianCV2345@users.noreply.github.com>
2025-11-24 22:30:02 -0500

    fix: descripcion basica tests arreglados.

--- src/Tests/DescripcionBasica.test.js
+++ src/Tests/DescripcionBasica.test.js
@@ -1,44 +1,12 @@
-import { JSDOM } from 'jsdom';
 import { MarcarEstado } from '../services/MarcarEstado.js';
 
-describe('MarcarEstado', () => {
-  let result;
-
-  beforeEach(() => {
-    const dom = new JSDOM(`
-      <!DOCTYPE html>
-      <html>
-        <body>
-          <input type="radio" name="estado" value="disponible">
-          <input type="radio" name="estado" value="adoptado">
-          <div id="resultEstadoMarc"></div>
-        </body>
-      </html>
-    `);
-
-    global.document = dom.window.document;
-    result = document.getElementById('resultEstadoMarc');
-  });
-
-  it('debería marcar el estado como "disponible"', () => {
-    const radio = document.querySelector('input[value="disponible"]');
-    radio.checked = true;
-    const event = new dom.window.Event('change');
-    radio.dispatchEvent(event);
-
-    const seleccionado = document.querySelector('input[name="estado"]:checked');
-    result.textContent = MarcarEstado(seleccionado.value);
-    expect(result.textContent).toBe('La mascota ha sido marcada como disponible para adopción.');
-  });
-
-  it('debería marcar el estado como "adoptado"', () => {
-    const radio = document.querySelector('input[value="adoptado"]');
-    radio.checked = true;
-    const event = new dom.window.Event('change');
-    radio.dispatchEvent(event);
-
-    const seleccionado = document.querySelector('input[name="estado"]:checked');
-    result.textContent = MarcarEstado(seleccionado.value);
-    expect(result.textContent).toBe('La mascota ha sido marcada como adoptado.');
-  });
+describe('Pruebas para la función MarcarEstado', () => {
+    it('Debería ser Marcar a la mascota como disponible', () => {
+        expect(MarcarEstado("disponible")).toBe('La mascota ha sido marcada como disponible para adopción.');
+    });
+     it('Debería ser Marcar a la mascota como en proceso de Adopcion', () => {
+        expect(MarcarEstado("en proceso")).toBe('La mascota ha sido marcada como en proceso para adopción.');
+    }); it('Debería ser Marcar a la mascota como adoptada', () => {
+        expect(MarcarEstado("adoptado")).toBe('La mascota ha sido marcada como adoptado.');
+    });
 });
