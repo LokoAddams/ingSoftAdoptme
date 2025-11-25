6d878c352e120c805fbb9a5030a2ca12afd6a02e
JoseAB32 <arias.balderrama.josmearia@gmail.com>
2025-11-24 22:07:23 -0400

    feat: se agrego endpoint Post SOlicitud

--- backend/api/solicitud.api.js
+++ backend/api/solicitud.api.js
@@ -33,6 +33,62 @@ SolicitudRouter.get("/", async (req, res) => {
 });
 
 
+// Post crear nueva solicitud
+SolicitudRouter.post("/", async (req, res) => {
+  try {
+    const { mascotaId, adoptanteNombre } = req.body;
+
+    if (!mascotaId || !adoptanteNombre) {
+      return res
+        .status(400)
+        .json({ message: "mascotaId y nombre del adoptante son obligatorios" });
+    }
+
+    if (!ObjectId.isValid(mascotaId)) {
+      return res.status(400).json({ message: "mascotaId inválido" });
+    }
+
+    const mRepo = mascotaRepo();
+    const sRepo = solicitudRepo();
+    const mascotaObjectId = new ObjectId(mascotaId);
+
+    const mascota = await mRepo.findOne({
+      where: { _id: mascotaObjectId },
+    });
+
+    if (!mascota) {
+      return res.status(404).json({ message: "Mascota no encontrada" });
+    }
+
+    if (mascota.estado !== "Disponible") {
+      return res
+        .status(400)
+        .json({ message: "La mascota no está disponible para adopción" });
+    }
+
+    const fecha = new Date();
+
+    // Creamos entity para Mongo
+    const nuevaSolicitud = sRepo.create({
+      mascotaId: mascota._id,
+      adoptanteNombre,
+      estado: "pendiente", 
+      fechaSolicitud: fecha,
+    });
+
+    const guardada = await sRepo.save(nuevaSolicitud);
+
+    mascota.estado = "pendiente";
+    await mRepo.save(mascota);
+
+    return res.status(201).json(mapSolicitudEntityToDto(guardada));
+  } catch (error) {
+    console.error("Error al crear solicitud:", error);
+    return res
+      .status(500)
+      .json({ message: "Error interno al crear solicitud" });
+  }
+});
 
 
 
